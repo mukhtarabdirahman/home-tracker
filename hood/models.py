0712fa0fafe3da3901ab70db631c29a62cd1cb03
mukhtarabdirahman <mukhtarabdirahmamn@gmail.com>
2020-02-26 14:30:28 +0300

    model profile

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    profilePic = models.ImageField(upload_to='profile/',null=True)
+    bio = models.CharField(max_length=60,blank=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+    
+    def create_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
