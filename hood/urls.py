d30fb91469d8b4379b6d75083e77c72e599ed0af
mukhtarabdirahman <mukhtarabdirahmamn@gmail.com>
2020-02-26 17:19:52 +0300

    uploading

--- hood/urls.py
+++ hood/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     url('^home/$',views.home,name = 'home'),
+    url(r'^image_form/$', views.image_form, name='image_form'),
 ]
--- hood/views.py
+++ hood/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Profile,Neighbourhood
+from .forms import UserRegisterForm, PostPictureForm
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def home(request):
     posts = Neighbourhood.objects.all()
     return render(request, 'index.html', { 'posts': posts })
+
+
+
+def image_form(request):
+    if request.method == 'POST': 
+        form = PostPictureForm(request.POST, request.FILES) 
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.profile = request.user.profile
+            form.save()
+            return redirect('home') 
+    else: 
+        form = PostPictureForm() 
+    return render(request, 'image_form.html', {'form' : form}) 
