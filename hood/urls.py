bf5800b1b774d85b4c3a0a43203403deedcb6ae5
mukhtarabdirahman <mukhtarabdirahmamn@gmail.com>
2020-02-27 07:00:06 +0300

    procfile

--- hood/urls.py
+++ hood/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^home/$',views.home,name = 'home'),
+    url('^$',views.home,name = 'home'),
     url(r'^image_form/$', views.image_form, name='image_form'),
 ]
